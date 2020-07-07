254f96ffec4bf104bf996b8f004a159e1034b4df
Pavel Paranin <paraninpasha@gmail.com>
2020-07-07 14:08:20 +0200

    empty strings can be handled properly

--- menus_handler.py
+++ menus_handler.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from flask import Flask
 from flask import request
 from parsers import create_menu_json
@@ -7,18 +5,18 @@ from parsers import create_menu_json
 app = Flask(__name__)
 
 
-def get_menu(restaurants: Optional[str]):
+def get_menu(restaurants: str):
     menu = create_menu_json(restaurants)
     return menu
 
 
 @app.route("/menu", methods=["POST"])
 def get_menu_normal_request():
-    restaurants = request.form.get("restaurants", None)
+    restaurants = request.form.get("restaurants", "").strip()
     return get_menu(restaurants)
 
 
 @app.route("/slack/menu", methods=["POST"])
 def get_menu_slack_request():
-    restaurants = request.form.get("text", None)
+    restaurants = request.form.get("text", "").strip()
     return get_menu(restaurants)
--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from parsers.abstract_parser import AbstractParser
 from parsers.bernard import BernardParser
 
@@ -12,8 +10,8 @@ def restaurant_factory(restaurant_name: str) -> AbstractParser:
     return restaurants_mapping.get(restaurant_name, AbstractParser)
 
 
-def create_menu_json(restaurants: Optional[str]) -> dict:
-    if restaurants is None:
+def create_menu_json(restaurants: str) -> dict:
+    if restaurants == "":
         separate_restaurants = list(restaurants_mapping.keys())
     else:
         separate_restaurants = restaurants.split(" ")
